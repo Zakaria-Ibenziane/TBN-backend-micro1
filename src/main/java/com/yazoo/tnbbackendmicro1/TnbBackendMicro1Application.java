2c88ebee37fa413daa872495d12120d747470f9f
jafariyounes561 <jafariyounes561@gmail.com>
2023-12-05 18:02:27 +0100

    first commit

---
+++ src/main/java/com/yazoo/tnbbackendmicro1/TnbBackendMicro1Application.java
@@ -0,0 +1,13 @@
+package com.yazoo.tnbbackendmicro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnbBackendMicro1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnbBackendMicro1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yazoo/tnbbackendmicro1/TnbBackendMicro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazoo.tnbbackendmicro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnbBackendMicro1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
